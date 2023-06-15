4eb4d39c12c0ae28a7ea2a0d6552a7fae5743728
Crowdbotics <team@crowdbotics.com>
2023-06-15 01:29:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "silent-king-41493",
+  url: "https://silent-king-41493.botics.co",
+  api: "https://silent-king-41493.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
